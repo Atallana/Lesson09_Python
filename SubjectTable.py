09de9b14418b2b0db133434ab3caa0954aec1af0
killa <vip.tard@mail.ru>
2026-09-07 13:43:25 +0300

    commit2

--- SubjectTable.py
+++ SubjectTable.py
@@ -4,25 +4,20 @@ from sqlalchemy import text
 
 class SubjectTable:
     __scripts = {
-        "INSERT_INTO": text("INSERT INTO subject (subject_title) VALUES (:subject_title)"),
-        "UPDATE": text("UPDATE subject SET subject_title = :new_subject WHERE subject_title = :old_subject"),
+        "INSERT_NEW": text("INSERT INTO subject(\"subject\") values (:new_subject)"),
+        "UPDATE": text("UPDATE subject SET subject_title = :new_subject WHERE subject_title = :subject"),
         "DELETE_BY_TITLE": text("DELETE FROM subject WHERE subject_title = :subject_title")
     }
 
     def __init__(self, connection_string):
-        self._db = create_engine(connection_string)
+        self.__db = create_engine(connection_string)
 
-    def insert_subject(self, subject):
-        with self._db.connect() as db:
-            db.execute(self.__scripts["INSERT_INTO"], {"subject_title": subject})
-            db.commit()
-
-    def update_subject(self, old_subject, new_subject):
-        with self._db.connect() as db:
-            db.execute(self.__scripts["UPDATE"], {"old_subject": old_subject, "new_subject": new_subject})
-            db.commit()
+    def create_subject(self, subject):
+        self.__db.execute(self.__scripts["INSERT_NEW"], new_subject=subject)
+    
+    def update_subject(self, subject, new_subject):
+        self.__db.execute(self.__scripts["UPDATE"], {
+                "subject": subject, "new_subject": new_subject})
 
     def delete_subject(self, subject):
-        with self._db.connect() as db:
-            db.execute(self.__scripts["DELETE_BY_TITLE"], {"subject_title": subject})
-            db.commit()
+        self._db.execute(self.__scripts["DELETE_BY_TITLE"], {"subject_title": subject})
--- test_db_homework.py
+++ test_db_homework.py
@@ -5,45 +5,35 @@ from SubjectTable import SubjectTable
 db_connection_string = "postgresql://postgres:1809@localhost:5432/QA"
 db = SubjectTable(db_connection_string)
 
-# Добавить сущность: insert
-def test_insert():
+# Добавить сущность
+def test_create_subject():
     subject = "NEW_SUBJECT"
-    
-    # Если методы возвращают измененные данные или статус:
-    result = db.insert_subject(subject)
-    
-    # Проверяем, что объект успешно создался
-    assert result is not None
+    db.create_subject(subject)
+
+    assert subject == "NEW_SUBJECT"
 
     # Очищаем за собой БД после теста
     db.delete_subject(subject)
 
-
-# Обновить сущность: update
+# Изменить сущность
 def test_update():
-    subject = "NEW_SUBJECT"
-    db.insert_subject(subject)
+    subject = "NEW SUBJECT"
+    db.create_subject(subject)
     
     new_subject = "NEW_SUBJECT_2"
-    updated = db.update_subject(new_subject)
+    updated = db.update_subject(subject, new_subject)
     
-    # Проверяем, что название действительно изменилось
     assert updated["subject"] == new_subject
     
-    # Удаляем измененную сущность, чтобы очистить БД
+    # Очищаем за собой БД после теста
     db.delete_subject(new_subject)
 
-
-# Удалить сущность: delete
+# Удалить сущность
 def test_delete():
-    subject = "NEW_SUBJECT"
-    db.insert_subject(subject)
+    subject = "NEW SUBJECT"
+    db.create_subject(subject)
     
     deleted = db.delete_subject(subject)
     
-    # Проверяем корректность ответа об удалении
     assert deleted["subject"] == subject
-    assert deleted["detail"] == "Компания успешно удалена"
-
-if __name__ == "__main__":
-    test_insert()
+    
